5d73564fdd588192a8fe176f3b6a769dbd8889e8
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-02-13 06:20:39 -0500

    extract global style/theme logic to GlobalStyles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 
 import { Metadata } from "next";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 
-import theme from '@/app/styles/theme';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import MainContainer from '@/components/MainContainer';
+import GlobalStyles from "./GlobalStyles";
 
 export const metadata: Metadata = {
   title: {
@@ -40,15 +37,11 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang='en'>
       <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Header />
-            <MainContainer>{children}</MainContainer>
-            <Footer />
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <GlobalStyles>
+          <Header />
+          <MainContainer>{children}</MainContainer>
+          <Footer />
+        </GlobalStyles>
       </body>
     </html>
   );
