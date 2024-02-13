d52755b779c93bcc1c31806f6e287dced0dfc9b1
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-02-13 06:49:57 -0500

    move global styles to styles dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import MainContainer from '@/components/MainContainer';
-import GlobalStyles from "./GlobalStyles";
+import GlobalStyles from "@/app/styles/GlobalStyles";
 
 export const metadata: Metadata = {
   title: {
