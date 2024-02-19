5bd4b5299da8b772fb023fad40ade343bf4fbac8
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-02-19 15:29:50 -0500

    write testing notes

--- src/app/work/page.test.tsx
+++ src/app/work/page.test.tsx
@@ -5,6 +5,12 @@ import { renderWithLayout, screen, renderSnapshotWithLayout } from 'test-utils';
 
 import WorkPage from './page';
 
+// Test TODO
+// Work header nav element is underlined
+// h1 is correct
+// each panel can be clicked and the expected elments are on the page
+// keyboard events work
+
 describe('The Work Page', () => {
   test('has expected snapshot', () => {
     const component = renderSnapshotWithLayout(<WorkPage />);
