de851229cf77ca2199eea2800de448039f667970
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-02-27 07:07:28 -0500

    about, work, and contact all use body2 (#42)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,21 +15,21 @@ const About: React.FC = () => {
       <Typography variant="h1" gutterBottom>
         About
       </Typography>
-      <Typography gutterBottom>
+      <Typography gutterBottom variant="body2">
         I live in Orlando with my family. I&apos;m a native Floridian and
         graduate of the University of Florida <i>(Go Gators!)</i>. Prior to our
         return to the sunshine state, we experienced life in both Georgia and
         Colorado. Colorado was a remarkable experience, and it left an indelible
         mark on my heart.
       </Typography>
-      <Typography gutterBottom>
+      <Typography gutterBottom variant="body2">
         Since 2014, I&apos;ve been practicing yoga. Beyond the physical
         benefits, I love how it connects me back into the physical body and
         serves as a form of moving meditation. Lately I&apos;ve been working on
         my inversions. It took me some time to gather the courage to embrace
         being upside down, but I&apos;m grateful I took that leap.
       </Typography>
-      <Typography gutterBottom>
+      <Typography gutterBottom variant="body2">
         Beyond the mat, I enjoy cooking, caring for my houseplants, and spending
         time in nature.
       </Typography>
