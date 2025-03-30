47dc1355a9c224ff783118739e1d5f905487fb36
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-03-30 16:47:05 -0400

    remove commented out code

--- src/app/ssrMediaQueries/getDeviceType.ts
+++ src/app/ssrMediaQueries/getDeviceType.ts
@@ -4,8 +4,6 @@ import { UAParser, IDevice } from 'ua-parser-js';
 
 const getDeviceType = async (): Promise<DeviceType> => {
   // Parse deviceType from user agent header on server
-  // const headersList = await (headers() as unknown as UnsafeUnwrappedHeaders);
-  // const userAgent = headersList.get('user-agent');
   const userAgent = (await headers()).get('user-agent');
   return getDeviceTypeFromUserAgent(userAgent);
 };
