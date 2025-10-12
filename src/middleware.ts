fd5679bdbf77ee70d71aefbade24c287de7389f8
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-12 19:28:00 -0400

    exclude sitemap.xml and robots.txt

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,8 +22,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - sitemap.xml
+     * - robots.txt
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 };
 
