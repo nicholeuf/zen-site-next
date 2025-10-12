ff05f889fd5bb9c4d567e256cfe6da69b9002b70
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-12 19:29:17 -0400

    default to the root path (/)

--- src/app/lib/getServerPath.ts
+++ src/app/lib/getServerPath.ts
@@ -1,7 +1,7 @@
 import { headers } from 'next/headers';
 
 const getServerPath = async (): Promise<string> => {
-  return (await headers()).get('x-next-pathname') || '';
+  return (await headers()).get('x-next-pathname') || '/';
 };
 
 export default getServerPath;
