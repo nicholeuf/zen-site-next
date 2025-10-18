573cda7d3fc195c626a2952b8dc14f83a4092103
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-18 19:10:49 -0400

    add a title to the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
+
 import NotFoundComponent from '@/components/NotFound';
+
+export const metadata: Metadata = {
+  title: '404 - Page Not Found',
+};
 
 const NotFound = () => {
   return <NotFoundComponent />;
