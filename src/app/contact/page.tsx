1a5f99c53a63c22a3f031bce4221e3d2a02cb850
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-18 08:07:20 -0400

    use ImageContainer on the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,33 +1,14 @@
 import { Metadata } from 'next';
 
 import ContactPage from './ContactPage';
-import ContactContent from './ContactContent';
 import { SMALLCHAT_ENABLED } from '@/app/lib/smallchat';
-import ImageDimensions from '@/types/ImageDimensions';
 
 export const metadata: Metadata = {
   title: 'Contact',
 };
 
-const dimensions: ImageDimensions = {
-  width: 368,
-  height: 644,
-};
-
 const Contact: React.FC = () => {
-  return (
-    <ContactPage
-      imageProps={{
-        src: 'zensite/shine-stephania-plant-in-plant-pot',
-        crop: 'fill',
-        opacity: '30',
-        alt: '',
-      }}
-      dimensions={dimensions}
-    >
-      <ContactContent chatEnabled={SMALLCHAT_ENABLED} />
-    </ContactPage>
-  );
+  return <ContactPage chatEnabled={SMALLCHAT_ENABLED} />;
 };
 
 export default Contact;
