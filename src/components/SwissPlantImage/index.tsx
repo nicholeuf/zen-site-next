b4a7c03d3af4432dae62f004c27b00bd4db0251b
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-17 20:09:32 -0400

    Swiss Cheese Plant Image

---
+++ src/components/SwissPlantImage/index.tsx
@@ -0,0 +1,47 @@
+import { css } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import { CldImage } from 'next-cloudinary';
+
+const SwissPlantImage = () => {
+  return (
+    <Box
+      data-testid="image-container"
+      sx={{
+        position: 'relative',
+        height: 'auto',
+        aspectRatio: '0.77',
+        overflow: 'hidden',
+      }}
+      css={(theme) => css`
+        width: 250px;
+        @media (min-width: 401px) {
+          width: 375px;
+        }
+        @media (min-width: ${theme.breakpoints.values.sm}px) {
+          width: 500px;
+        }
+      `}
+    >
+      <CldImage
+        src="zensite/lucila-naves-swiss-guava"
+        opacity="50"
+        alt=""
+        fill={true}
+        aspectRatio="0.77"
+        sizes="50vw"
+        data-testid="bg-image"
+        crop="scale"
+        style={{
+          objectFit: 'contain',
+          position: 'absolute',
+          top: '0',
+          right: '0',
+          overflow: 'hidden',
+          zIndex: -1,
+        }}
+      />
+    </Box>
+  );
+};
+
+export default SwissPlantImage;
