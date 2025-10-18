967ae066a4ca91d3a970ec49b24248fdbd647b09
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-18 14:38:10 -0400

    adjust media queries for smaller screen sizes

--- src/components/SwissPlantImage.tsx
+++ src/components/SwissPlantImage.tsx
@@ -20,9 +20,9 @@ const SwissPlantImage = () => {
         overflow: 'hidden',
       }}
       css={(theme: Theme) => `
-        width: 250px;
+        width: 175px;
         @media (min-width: 401px) {
-          width: 375px;
+          width: 300px;
         }
         @media (min-width: ${theme.breakpoints.values.sm}px) {
           width: 500px;
