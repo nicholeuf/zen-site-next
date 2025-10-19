e545ffcb2baaba0639af6130a1561b751042b9df
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-18 20:14:21 -0400

    remove comment

--- src/app/styles/constants.tsx
+++ src/app/styles/constants.tsx
@@ -9,7 +9,7 @@ const constants = {
     guava: '#fa2742',
     carob: '#373833',
     cream: '#f8faf3',
-    gunmetal: '#2b303a', // if alpha was unintended; otherwise document and consistently use 8-digit format
+    gunmetal: '#2b303a',
   },
 };
 
