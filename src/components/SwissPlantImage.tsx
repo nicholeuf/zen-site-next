d5a2e336cf35b9d078e4fe1c121a2757530edd5c
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-22 14:01:51 -0400

    Implement code suggestion

--- src/components/SwissPlantImage.tsx
+++ src/components/SwissPlantImage.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
 /** @jsxImportSource @emotion/react */
-
+import { css } from '@mui/material';
 import Box from '@mui/material/Box';
 import { CldImage } from 'next-cloudinary';
 
@@ -14,14 +14,15 @@ const SwissPlantImage = () => {
         aspectRatio: '0.77',
         overflow: 'hidden',
       }}
-      css={(theme) => `
+      css={css`
         width: 175px;
         @media (min-width: 401px) {
           width: 300px;
         }
-        @media (min-width: ${theme.breakpoints.values.sm}px) {
+        @media (min-width: 600px) {
           width: 500px;
         }
+        height: auto;
       `}
     >
       <CldImage
