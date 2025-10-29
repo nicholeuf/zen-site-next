e005b190558d2304cdf77eb74e6f0f1b1901777b
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-10-29 13:39:06 -0400

    storybook initial setup

---
+++ .storybook/preview.ts
@@ -0,0 +1,41 @@
+import type { Preview } from '@storybook/nextjs-vite';
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import theme from '../src/app/styles/theme';
+import GlobalStyles from '../src/app/styles/GlobalStyles';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo',
+    },
+    //👇 Enables auto-generated documentation for all stories
+    tags: ['autodocs'],
+  },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      GlobalStyles,
+      Provider: ThemeProvider,
+      themes: {
+        desktop: theme('desktop'),
+        mobile: theme('mobile'),
+        tablet: theme('tablet'),
+      },
+      defaultTheme: 'desktop',
+    }),
+  ],
+};
+
+export default preview;
