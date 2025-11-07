2dc227cf964a10c477e1ee13e958f3696445d6c7
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-11-07 16:44:18 -0500

    header logo spacing affordances for the focus visible outline

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -18,7 +18,7 @@ const HeaderLogo: React.FC<HeaderLogoProps> = ({
       sx={{
         typography: 'sacramento',
         fontSize: '30px',
-        height: '100%',
+        height: width,
         width,
         display: 'flex',
         justifyContent: 'center',
@@ -26,6 +26,7 @@ const HeaderLogo: React.FC<HeaderLogoProps> = ({
         '&:hover': {
           cursor: 'pointer',
         },
+        p: 1,
       }}
       data-testid="header-logo"
     >
@@ -34,6 +35,7 @@ const HeaderLogo: React.FC<HeaderLogoProps> = ({
         sx={{
           color,
           width: '100%',
+          height: '100%',
           textAlign: 'center',
           '&:hover': {
             color: activeColor,
@@ -41,6 +43,9 @@ const HeaderLogo: React.FC<HeaderLogoProps> = ({
           },
           '&::before': {
             content: '"nf"',
+            width: '100%',
+            height: '100%',
+            display: 'block',
           },
         }}
       >
