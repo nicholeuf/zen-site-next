7fd9093cb11624d50529c8b66f07bc412a0bb171
Nichole Frey <nicholeuf@users.noreply.github.com>
2025-12-12 15:08:38 -0500

    Handled the Sentry warnings by updating next.config.ts to the new option names: disableLogger now lives under webpack.treeshake.removeDebugLogging, and automaticVercelMonitors is now under webpack.automaticVercelMonitors

--- next.config.ts
+++ next.config.ts
@@ -50,12 +50,16 @@ export default withSentryConfig(nextConfig, {
   // side errors will fail.
   tunnelRoute: "/monitoring",
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  webpack: {
+    treeshake: {
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      removeDebugLogging: true,
+    },
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
 });
