45bf63855ec39589c72ab0f74e3769f5c4ec21fe
Nichole Frey <nicholeuf@users.noreply.github.com>
2026-02-09 12:58:06 -0500

    add comment as to why the not-found.tsx server component is doing superficial testing

--- src/app/not-found.test.tsx
+++ src/app/not-found.test.tsx
@@ -6,7 +6,9 @@ import NotFound from "./not-found";
 describe("The Not Found (404) Route", () => {
   test("renders the not-found container", () => {
     renderWithLayout(<NotFound />);
-
     expect(document.getElementById("not-found")).toBeInTheDocument();
+    // At this level, just test that the not-found container is rendered.
+    // The NotFound component itself is tested in its own story, so we can
+    // be confident that if the container is present, the content is as well.
   });
 });
