5cf2aa101a1d90b0c7ec10721ccc5d40c71b992e
Nichole Frey <nicholeuf@users.noreply.github.com>
2026-02-09 23:14:46 -0500

    landing page / home page stories now contain interactions from tests

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -2,25 +2,11 @@
 import navigationMocks from "utils/nextNavigationMock";
 import mockGetPlaceholderImage from "./lib/getPlaceholderImage.mock";
 
-import userEvent from "@testing-library/user-event";
-import {
-  MD_DEVICE,
-  renderSnapshotWithLayout,
-  renderWithLayout,
-  resetMatchMedia,
-  resolvedComponent,
-  screen,
-  waitFor,
-  XS_DEVICE,
-} from "test-utils";
+import { renderWithLayout, resolvedComponent, screen } from "test-utils";
 import { vi } from "vitest";
 import HomePage from "./page";
 
 describe("The Home Page", () => {
-  beforeAll(() => {
-    resetMatchMedia();
-  });
-
   beforeEach(() => {
     // set pathname for the next/navigation mock
     navigationMocks.usePathname.mockImplementation(() => "/");
@@ -30,97 +16,15 @@ describe("The Home Page", () => {
   afterEach(() => {
     vi.resetAllMocks();
   });
-
-  test("has expected snapshot", async () => {
+  test.each([
+    ["header"],
+    ["landing"],
+    ["footer"],
+  ])("contains the visible testid %p", async (testid) => {
     const HomeResolved = await resolvedComponent(HomePage);
-    const { container } = renderSnapshotWithLayout(<HomeResolved />);
-    expect(container).toMatchSnapshot();
-  });
-
-  describe("User with MD Device", () => {
-    beforeAll(() => {
-      resetMatchMedia(MD_DEVICE);
-    });
-
-    test("can interact with navigation", async () => {
-      const HomeResolved = await resolvedComponent(HomePage);
-      renderWithLayout(<HomeResolved />);
-
-      // The home link in the top left corner
-      expect(
-        screen.getAllByRole("link", {
-          name: "Home",
-        })[0]
-      ).toBeVisible();
-
-      expect(
-        screen.getByRole("navigation", {
-          name: "Main Navigation",
-        })
-      ).toBeVisible();
-
-      const workLink = screen.getByRole("link", {
-        name: "Work",
-      });
-      expect(workLink).not.toHaveAttribute("aria-current", "page");
-      const homeLink = screen.getAllByRole("link", {
-        name: "Home",
-      })[1];
-      expect(homeLink).toHaveAttribute("aria-current", "page");
-    });
-  });
-
-  describe("User with XS Device", () => {
-    beforeAll(() => {
-      resetMatchMedia(XS_DEVICE);
-    });
-
-    test("can interact with mobile navigation", async () => {
-      const HomeResolved = await resolvedComponent(HomePage);
-      renderWithLayout(<HomeResolved />);
-
-      const user = userEvent.setup();
-      const getDialog = () => screen.queryByTestId("mobile-navigation-dialog");
-
-      // Dialog is not initially present in document
-      expect(getDialog()).not.toBeInTheDocument();
-
-      const openButton = screen.getByRole("button", {
-        name: "Mobile Navigation Trigger",
-      });
-      await waitFor(() => expect(openButton).toHaveFocus());
-      expect(openButton).toHaveAttribute("aria-expanded", "false");
-
-      // Clicking the open button opens the dialog
-      await user.click(openButton);
-      expect(getDialog()).toBeVisible();
-      expect(openButton).toHaveAttribute("aria-expanded", "true");
-
-      const closeButton = screen.getByRole("button", {
-        name: "Close Mobile Navigation",
-      });
-      await waitFor(() => expect(closeButton).toHaveFocus());
-
-      expect(
-        screen.getByRole("navigation", {
-          name: "Main Navigation",
-        })
-      ).toBeVisible();
-
-      const workLink = screen.getByRole("link", {
-        name: "Work",
-      });
-      expect(workLink).not.toHaveAttribute("aria-current", "page");
-      const homeLink = screen.getByRole("link", {
-        name: "Home",
-      });
-      expect(homeLink).toHaveAttribute("aria-current", "page");
-
-      // Clicking the close button closes the dialog
-      await user.click(closeButton);
-      await waitFor(() => expect(getDialog()).not.toBeInTheDocument());
-      expect(openButton).toHaveFocus();
-      expect(openButton).toHaveAttribute("aria-expanded", "false");
-    });
+    renderWithLayout(<HomeResolved />);
+    // renderWithLayout(<HomePage />);
+    const component = screen.getByTestId(testid);
+    expect(component).toBeVisible();
   });
 });
--- utils/StoryAppLayout.tsx
+++ utils/StoryAppLayout.tsx
@@ -1,9 +1,17 @@
 import MainContainer from "../src/components/MainContainer";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const StoryAppLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <MainContainer>{children}</MainContainer>;
+  return (
+    <>
+      <Header />
+      <MainContainer>{children}</MainContainer>
+      <Footer />
+    </>
+  );
 };
 
 export default StoryAppLayout;
