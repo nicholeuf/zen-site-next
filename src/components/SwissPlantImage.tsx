88a8644ef5cb80d2f3055018542c382721697055
Nichole Frey <nicholeuf@users.noreply.github.com>
2026-06-01 16:57:45 -0400

    swissplantimage has a dark mode friendly version

--- src/components/SwissPlantImage.tsx
+++ src/components/SwissPlantImage.tsx
@@ -1,10 +1,15 @@
+"use client";
 /* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
 /** @jsxImportSource @emotion/react */
 import { css } from "@mui/material";
 import Box from "@mui/material/Box";
 import { CldImage } from "next-cloudinary";
+import { useCurrentMode } from "../app/hooks/useCurrentMode";
 
 const SwissPlantImage = () => {
+  const currentMode = useCurrentMode();
+  const isDark = currentMode === "dark";
+
   return (
     <Box
       data-testid="swiss-plant-image"
@@ -25,6 +30,7 @@ const SwissPlantImage = () => {
         height: auto;
       `}
     >
+      {/* Light version */}
       <CldImage
         src="zensite/lucila-naves-swiss-guava"
         opacity="60"
@@ -39,8 +45,32 @@ const SwissPlantImage = () => {
           position: "absolute",
           top: "0",
           right: "0",
-          overflow: "hidden",
           zIndex: -1,
+          transition: "opacity 700ms ease-in-out",
+          opacity: isDark ? 0 : 1,
+        }}
+      />
+
+      {/* Dark version */}
+      <CldImage
+        src="zensite/lucila-naves-swiss-guava"
+        alt=""
+        fill={true}
+        aspectRatio="0.77"
+        sizes="50vw"
+        data-testid="bg-image-dark"
+        crop="scale"
+        brightness="45"
+        contrast="20"
+        tint="equalize:55:rgb:f0e6d2"
+        style={{
+          objectFit: "contain",
+          position: "absolute",
+          top: "0",
+          right: "0",
+          zIndex: -1,
+          transition: "opacity 700ms ease-in-out",
+          opacity: isDark ? 1 : 0,
         }}
       />
     </Box>
